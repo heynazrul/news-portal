2cd3b68f6e0dd8bfacf645faa0553a7f314aca50
Nazrul <heynazrul@gmail.com>
2023-03-02 15:36:49 +0600

    added news details modal

--- scripts/news-portal.js
+++ scripts/news-portal.js
@@ -32,7 +32,7 @@ const displayAllNews = (data, category_name) => {
   const newsContainer = document.getElementById('all-news');
   newsContainer.innerHTML = '';
   data.forEach((singleNews) => {
-    const { image_url, details, author, total_view } = singleNews;
+    const { _id, image_url, details, author, total_view } = singleNews;
 
     newsContainer.innerHTML += `
     <div class="card mb-3">
@@ -65,7 +65,10 @@ const displayAllNews = (data, category_name) => {
                 <i class="fas fa-star"></i>
             </div>
             <div>
-                <i class="fas fa-arrow-right"></i>
+              <button class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#newsDetails"
+              onclick="fetchNewsDetails('${_id}')">
+              <i class="fas fa-arrow-right" ></i>
+              </button>
             </div>
           </div>
         </div>
@@ -73,4 +76,56 @@ const displayAllNews = (data, category_name) => {
     </div>
     `;
   });
+};
+
+// fetch news details
+const fetchNewsDetails = (news_id) => {
+  const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showNewsDetails(data.data[0]));
+};
+
+const showNewsDetails = (newsDetail) => {
+  console.log(newsDetail);
+  const { _id, title, image_url, details, author, total_view } = newsDetail;
+  console.log(title);
+
+  const modalTitle = document.getElementById('newsDetailsLabel');
+  const modalBody = document.getElementById('modal-body');
+  modalTitle.innerText = title;
+  modalBody.innerHTML = `
+  <div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-12">
+          <img src="${image_url}" class="img-fluid rounded-start" alt="..." />
+        </div>
+        <div class="col-md-12 d-flex flex-column">
+          <div class="card-body">
+            <p class="card-text">
+              ${details}
+            </p>
+          </div>
+          <div class="card-footer border-0 bg-body d-flex justify-content-between align-items-center">
+            <div class="d-flex gap-2">
+              <img src=${author.img} class="img-fluid rounded-circle" alt="..." height="40" width="40" />
+              <div>
+                <p class="p-0 m-0">${author.name}</p>
+                <p class="p-0 m-0">${author.published_date}</p>
+              </div>
+            </div>  
+            <div class="d-flex align-items-center gap-2">
+                <i class="fas fa-eye"></i>
+                <p class="p-0 m-0">${total_view}</p>
+            </div>
+            <div>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
 };
