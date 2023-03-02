aa791f88d65c4898a7f64b8a2f716fc85149aa15
Nazrul <heynazrul@gmail.com>
2023-03-02 14:43:31 +0600

    added all news by api call

--- scripts/news-portal.js
+++ scripts/news-portal.js
@@ -10,7 +10,8 @@ const displayCategories = (data) => {
   const categoriesContainer = document.getElementById('categories-container');
   data.forEach((category) => {
     categoriesContainer.innerHTML += `
-        <a class="nav-link" href="#" onclick="fetchCategoryNews('${category.category_id}','${category.category_name}')">${category.category_name}</a>
+        <a class="nav-link" href="#" onclick="fetchCategoryNews('${category.category_id}','${category.category_name}')">
+        ${category.category_name}</a>
     `;
   });
 };
@@ -24,10 +25,52 @@ const fetchCategoryNews = (category_id, category_name) => {
 };
 
 const displayAllNews = (data, category_name) => {
-  console.log(data);
-
   const newsCount = document.getElementById('news-count');
   const categoryName = document.getElementById('category-name');
   newsCount.innerText = data.length;
   categoryName.innerText = category_name;
+  const newsContainer = document.getElementById('all-news');
+  newsContainer.innerHTML = '';
+  data.forEach((singleNews) => {
+    const { image_url, details, author, total_view } = singleNews;
+
+    newsContainer.innerHTML += `
+    <div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="${image_url}" class="img-fluid rounded-start" alt="..." />
+        </div>
+        <div class="col-md-8 d-flex flex-column">
+          <div class="card-body">
+            <h5 class="card-title">${singleNews.title}</h5>
+            <p class="card-text">
+              ${details.slice(0, 300)}...
+            </p>
+          </div>
+          <div class="card-footer border-0 bg-body d-flex justify-content-between align-items-center">
+            <div class="d-flex gap-2">
+              <img src=${author.img} class="img-fluid rounded-circle" alt="..." height="40" width="40" />
+              <div>
+                <p class="p-0 m-0">${author.name}</p>
+                <p class="p-0 m-0">${author.published_date}</p>
+              </div>
+            </div>  
+            <div class="d-flex align-items-center gap-2">
+                <i class="fas fa-eye"></i>
+                <p class="p-0 m-0">${total_view}</p>
+            </div>
+            <div>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <div>
+                <i class="fas fa-arrow-right"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+  });
 };
